ad4a70c9af96a5dae2962fcaa3032a84f10ca617
Mauhwi <tykityki7@gmail.com>
2026-05-09 20:59:51 +0000

    oops

--- resources/js/Pages/Projects/Show.vue
+++ resources/js/Pages/Projects/Show.vue
@@ -1,15 +1,38 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3';
 import { usePage } from "@inertiajs/vue3";
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const page  = usePage();
 
-const props = defineProps({
+const { project } = defineProps({
   project: Object
 });
+
+const chaptersByPart = computed(() => {
+    return project.parts.map(part => {
+        return {
+            ...part,
+
+            chapters: project.chapters.filter(chapter =>
+                chapter.part_id === part.id
+            )
+        }
+    })
+})
 </script>
 
 <template>
-  <div class="container">{{ props.project.title }}</div>
+  <div class="container">
+    <h2>{{ project.title }}</h2>
+    <div v-for="part in chaptersByPart" :key="part.id">
+      <h3>{{ part.title }}</h3>
+      <div
+          v-for="chapter in part.chapters"
+          :key="chapter.id"
+      >
+          {{ chapter.title }}
+      </div>
+    </div>
+  </div>
 </template>
